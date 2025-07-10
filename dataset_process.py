754785ba3706de1f2209895ea4a3dcc54aaba142
luzeeliu <luzeliu1207@gmail.com>
2025-07-10 19:16:19 +1000

    # dataset download

--- dataset_process.py
+++ dataset_process.py
@@ -1,6 +1,16 @@
 import kagglehub
-
+import os
 # Download latest version
+"""
 path = kagglehub.dataset_download("anushabellam/trending-videos-on-youtube")
 
 print("Path to dataset files:", path)
+"""
+from datasets import load_dataset
+
+path = os.path.join("dataset")
+# Login using e.g. `huggingface-cli login` to access this dataset
+ds = load_dataset(
+    "AdoCleanCode/Youtube8M_real_train_data_v4_0.8",
+    cache_dir=path              
+)
